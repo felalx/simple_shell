08501c815284e7df2a1b55314b5141671f08b537
chernor <cherinhokendrick@gmail.com>
2023-09-20 16:11:55 -0700

    Chernor: shell.c update

--- shell.c
+++ shell.c
@@ -1,32 +1,48 @@
-#include "shell.h"
-
+#include "main.h"
 /**
- * main - This is for Entry point
- * @argc: for argument count
- * @argv: for argument vector
- *
- * Return: 0 for success, others for failure
+ * main - main function
+ * @ac: argument count
+ * @av: argument vector
+ * Return: On success 1.
+ * On error, -1 is returned.
  */
-int main(int argc, char *argv[])
+
+int main(int ac, char **av)
 {
-	shell = argv[0];
-	shell_index = 0;
-	status = 0;
+	char *lineptr;
+	int i;
+	char **array;
+	size_t n = 0;
 
-	(void)argc;
-	while (++shell_index)
+	lineptr = NULL;
+	handle_args(ac, av);
+	while (1)
 	{
-		print_prompt("#cisfun$ ");
-
-		_getline(&line);
-
-		command = strtoarr(line, ' ');
-
-		status = execute_command();
-
-		free(line);
-		free_array(command);
+	if (!read_input_line(&lineptr, &n))
+	{
+	continue;
 	}
-
-	return (status);
+	if (lineptr[0] == '^' && lineptr[1] == '[')
+	{
+	continue;
+	}
+	array = tokenize_input(lineptr, &i);
+	if (!array)
+	{
+	continue;
+	}
+	if (check_exit(array))
+	{
+	free(lineptr);
+	exit(0);
+	}
+	if (_strcmp(array[0], "env") == 0)
+	{
+	handle_env();
+	free(array);
+	continue;
+	}
+	execute_command(array, av[0]);
+	free(array);
+	}
 }
